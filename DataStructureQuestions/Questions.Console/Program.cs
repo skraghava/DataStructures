851d5daae9c43210236abdefb627384a6b1abd49
skraghava <skraghava@gmail.com>
2020-01-03 20:43:54 -0500

    Main Programme

--- DataStructureQuestions/Questions.Console/Program.cs
+++ DataStructureQuestions/Questions.Console/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Questions.Console
+
+namespace Questions.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //var n = 1200;
+            //Console.WriteLine((int)((Math.Log10(n & -n))
+            //    / Math.Log10(2)) + 1);
+            Console.ReadKey();
         }
     }
 }
