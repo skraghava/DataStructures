3e5a1dc16291aa4fe2e00785dbed44a71abef9fd
skraghava <skraghava@gmail.com>
2020-01-03 22:41:48 -0500

    Right most different bit

---
+++ DataStructureQuestions/Questions.UnitTest/RightMostDifferentBitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Questions.UnitTest
+{
+    [TestClass]
+    public class RightMostDifferentBitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RightMostDifferentBit rightMost = new RightMostDifferentBit();
+            Assert.AreEqual(-1, rightMost.GetRightMostDifferentBit(1,1)); // 0001
+            Assert.AreEqual(1, rightMost.GetRightMostDifferentBit(2,3)); // 0010 , 0011
+            Assert.AreEqual(3, rightMost.GetRightMostDifferentBit(3,7)); // 0011 0111
+            Assert.AreEqual(5, rightMost.GetRightMostDifferentBit(4,20)); // 0100
+            Assert.AreEqual(4, rightMost.GetRightMostDifferentBit(5,13)); // 0101 1101            
+        }
+    }
+}
---
+++ DataStructureQuestions/Questions/RightMostDifferentBit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    /// <summary>
+    /// Given two numbers number1 and number2. find the position of rightmost different bit in binary representation of these numbers
+    /// </summary>
+    public class RightMostDifferentBit
+    {
+        /// <summary>
+        /// xor sets the bit where deviation is there.
+        /// So take xor and then find the right most set bit
+        /// </summary>
+        /// <param name="number1"></param>
+        /// <param name="number2"></param>
+        /// <returns></returns>
+        public int GetRightMostDifferentBit(int number1, int number2)
+        {
+            if (number1 == number2)
+                return -1;
+            RightMostSetBit rightMostSetBit = new RightMostSetBit();
+
+            return rightMostSetBit.GetRightMostSetBit1(number1 ^ number2);
+        }
+    }
+}
