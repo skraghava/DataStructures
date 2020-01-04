e23a1d20c7a9fca9bd8610a75cb7053e59b2dde9
skraghava <skraghava@gmail.com>
2020-01-03 23:39:57 -0500

    Is Nth bit set or not

---
+++ DataStructureQuestions/Questions.UnitTest/NthBitSetTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Questions.UnitTest
+{
+    [TestClass]
+    public class NthBitSetTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            NthBitSet nthBitSet = new NthBitSet();
+            Assert.AreEqual(false, nthBitSet.IsNthBitSet(1,33));
+            Assert.AreEqual(false, nthBitSet.IsNthBitSet(1, 0));
+            Assert.AreEqual(true, nthBitSet.IsNthBitSet(1, 1));
+            Assert.AreEqual(true, nthBitSet.IsNthBitSet(2, 2));
+            Assert.AreEqual(true, nthBitSet.IsNthBitSet(4, 3));
+            Assert.AreEqual(true, nthBitSet.IsNthBitSet(8, 4));
+            Assert.AreEqual(true, nthBitSet.IsNthBitSet(16, 5));
+            Assert.AreEqual(false, nthBitSet.IsNthBitSet(2, 1));
+            Assert.AreEqual(false, nthBitSet.IsNthBitSet(4, 4));
+            Assert.AreEqual(false, nthBitSet.IsNthBitSet(8, 8));
+            Assert.AreEqual(false, nthBitSet.IsNthBitSet(16, 16));
+        }
+    }
+}
---
+++ DataStructureQuestions/Questions/NthBitSet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    /// <summary>
+    /// Check to see if Nth bit (From Right) is set or not in binary representation of a given number
+    /// </summary>
+    public class NthBitSet
+    {
+        public bool IsNthBitSet(int number, int position)
+        {
+            if (position > 32 || position < 1)
+                return false;
+            return (number & (1 << position-1)) > 0;
+        }
+    }
+}
